5e35bd3c3eb5b844039a2a97a10cbbf51cc9a723
lyxHanHan <yixuan.li1@thoughtworks.com>
2020-11-04 23:00:23 +0800

    modify user 、modify register function、add signIn function and add some check

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/User.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/User.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capacity.gtb.mvc;
 
+import com.sun.javafx.beans.IDProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,13 +9,16 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    @NotNull(message = "username must not be null")
-    @Length(min=3,max=10,message = "Please set a username of 3-10 digits")
+    private Integer id;
+
+    @NotBlank(message = "username must not be null")
+    @Pattern(regexp = "^[0-9a-zA-Z_]{3,10}$",message = "Can only contain letters, numbers, underscores,and the length is 3-10 digits")
     private String username;
 
     @NotNull
--- src/main/java/com/thoughtworks/capacity/gtb/mvc/UserService.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/UserService.java
@@ -6,41 +6,58 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService {
-    private Map<Integer,User> userMap = new HashMap<>();
+    private Map<Integer, User> userMap = new HashMap<>();
 
-    public UserService(){
+    public UserService() {
 
 
-        userMap.put(1,new User("xiaoming","1234567","11111111111@qq.com"));
-        userMap.put(2,new User("xiaoli","1234567","11111111111@qq.com"));
+        userMap.put(1, new User(1, "xiaoming", "1234567", "11111111111@qq.com"));
+        userMap.put(2, new User(2, "xiaoli", "1234567", "11111111111@qq.com"));
     }
 
-    private Integer getId(){
+    private Integer getId() {
         return userMap.keySet().stream().sorted(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 return o1 - o2;
             }
-        }).collect(Collectors.toList()).get(0)+1;
+        }).collect(Collectors.toList()).get(0) + 1;
     }
 
     public void registUser(User user) {
         Integer id = getId();
-        userMap.put(id,user);
+        userMap.put(id, user);
     }
 
     public User signIn(String username, String password) {
-        User user = userMap.get(1);
-        if (user == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "can not found this user");
-        }
-        return user;
-    }
+//        for (User value : userMap.values()) {
+//            if (username == value.getUsername()) {
+//                User user = userMap;
+//            }
+//        }
 
+        List<User> userList = userMap.values().stream()
+                .filter(User -> User.getUsername().equals(username) && User.getPassword().equals(password)).collect(Collectors.toList());
+
+        if (userList == null|| userList.size()==0) {
+            throw new UserNotFoundException("用户名或者密码错误");
+        } else {
+            return userList.get(0);
+        }
     }
+}
+//        User user = userMap.get(1);
+//        if (user == null) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "can not found this user");
+//        }
+//        return user;
+//    }
+//
+//    }
 
